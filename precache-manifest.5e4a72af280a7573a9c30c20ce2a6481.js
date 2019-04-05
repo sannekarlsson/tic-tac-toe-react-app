c808673cf1ba73e2453c2dd04e13db63fca3012d
Sanne Karlsson <44125795+sannekarlsson@users.noreply.github.com>
2019-04-05 15:23:14 +0200

    Updates

---
+++ precache-manifest.5e4a72af280a7573a9c30c20ce2a6481.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3e243be759f60109433",
+    "url": "/tic-tac-toe-react-app/static/js/runtime~main.340e1403.js"
+  },
+  {
+    "revision": "060b78e2ed09e6cfee53",
+    "url": "/tic-tac-toe-react-app/static/js/main.4fb297c6.chunk.js"
+  },
+  {
+    "revision": "685d0c597e8419129599",
+    "url": "/tic-tac-toe-react-app/static/js/2.0e3f6a1a.chunk.js"
+  },
+  {
+    "revision": "060b78e2ed09e6cfee53",
+    "url": "/tic-tac-toe-react-app/static/css/main.728c7870.chunk.css"
+  },
+  {
+    "revision": "a0acc76dfb8cb5b5748724ebd3c4c2aa",
+    "url": "/tic-tac-toe-react-app/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/tic-tac-toe-react-app/precache-manifest.5e4a72af280a7573a9c30c20ce2a6481.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/tic-tac-toe-react-app/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
